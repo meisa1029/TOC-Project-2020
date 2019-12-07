c92043311381b7df9ecd563d9225b5cff1795a21
meisa1029 <meisa1029@yahoo.com.tw>
2019-12-07 01:46:07 -0500

    add menu

--- fsm.py
+++ fsm.py
@@ -73,7 +73,7 @@ class TocMachine(GraphMachine):
         print("random")
         reply_token = event.reply_token
         send_text_message(reply_token, "輸入想選擇的類別\n類別: 肉、菜、配菜、湯、其它")
-	state = 1
+        state = 1
         self.go_back()
 
     def on_enter_meat(self, event):
