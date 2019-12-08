2d6c4037fe4fd2f9e53baa03a9fddcfc4405ce8f
meisa1029 <meisa1029@yahoo.com.tw>
2019-12-07 22:19:40 -0500

    try button

--- fsm.py
+++ fsm.py
@@ -40,7 +40,7 @@ def check(t_name, s):
     return index
 
 #menu_template = TemplateSendMessage(alt_Text= "this", template = ButtonsTemplate(text =  "1/2", actions = [MessageTemplateAction(label = "1", text = "1"), MessageTemplateAction(label = "2", text = "2")]))
-menu_template ="alt_Text= 'this', template = ButtonsTemplate(text =  '1/2', actions = [MessageTemplateAction(label = '1', text = '1'), MessageTemplateAction(label = '2', text = '2')])
+menu_template ="alt_Text= 'this', template = ButtonsTemplate(text =  '1/2', actions = [MessageTemplateAction(label = '1', text = '1'), MessageTemplateAction(label = '2', text = '2')])"
 
 
 class TocMachine(GraphMachine):
