24e83ae1b793fd514f06fd3a0b59963e618eeaf6
meisa1029 <meisa1029@yahoo.com.tw>
2019-12-08 01:21:19 -0500

    add button

--- fsm.py
+++ fsm.py
@@ -96,7 +96,7 @@ class TocMachine(GraphMachine):
         print("menu")
         reply_token = event.reply_token
         #send_text_message(reply_token, "輸入\"1\": 進入選擇模式\n輸入\"2\": 進入輸入/刪除模式")
-        test = ["主選單", "煮什麼：幫你選擇菜單", "新增/刪除：新增刪除菜單"]
+        text = ["主選單", "煮什麼：幫你選擇菜單", "新增/刪除：新增刪除菜單"]
         buttons = ["煮什麼", "新增刪除"] 
         send_button_message(reply_token, text, buttons)
        # self.go_back()
