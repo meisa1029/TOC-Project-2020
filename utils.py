9b75ca295a40dde91ec655deeb6cbaf0a59df856
meisa1029 <meisa1029@yahoo.com.tw>
2019-12-07 23:08:48 -0500

    try button

--- utils.py
+++ utils.py
@@ -15,7 +15,23 @@ def send_text_message(reply_token, text):
 
 def send_button_message(reply_token):
     line_bot_api = LineBotApi(channel_access_token)
-    menu_template = TemplateSendMessage(alt_Text= 'this', template = ButtonsTemplate(text =  '1/2', actions = [MessageTemplateAction(label = '1', text = '1'), MessageTemplateAction(label = '2', text = '2')]))
+    menu_template = TemplateSendMessage(
+        alt_Text= "button template",
+        template = ButtonsTemplate(
+            title = "menu",
+            text =  "1/2",
+            actions = [
+                MessageTemplateAction(
+                    label = '1',
+                    text = '1'
+                ),
+                MessageTemplateAction(
+                    label = '2',
+                    text = '2'
+                )
+            ]
+        )
+    )
     line_bot_api.reply_message(reply_token, menu_template)
 """
 def send_image_url(id, img_url):
